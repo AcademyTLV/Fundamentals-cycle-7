c97dc9e1925f72e4098564438dd614e40da7a290
Yamin <Yamin.elmakis@here.com>
2019-11-04 21:02:42 +0200

    ex-2-views

--- app/src/main/java/com/android/academy/DetailsActivity.kt
+++ app/src/main/java/com/android/academy/DetailsActivity.kt
@@ -1,13 +1,22 @@
 package com.android.academy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        details_btn_trailer.setOnClickListener {
+            val trailerUrl = getString(R.string.infinity_war_trailer)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(trailerUrl))
+            startActivity(browserIntent)
+        }
     }
 
 }
