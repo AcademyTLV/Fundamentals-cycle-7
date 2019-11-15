a37a8a903ae8834263263e0350fe5a655cb37ba3
Vova Hotyakov <vovadbank@gmail.com>
2019-11-15 09:12:57 +0200

    redy for review ex_6

---
+++ app/src/main/java/com/android/academy/BackgroundServices/HardJobIntentService.kt
@@ -0,0 +1,46 @@
+package com.android.academy.BackgroundServices
+
+import android.app.IntentService
+import android.content.Intent
+import android.os.SystemClock
+
+import com.android.academy.R
+
+
+open class HardJobIntentService : IntentService(TAG) {
+
+    companion object {
+        private const val TAG = "HardJobIntentService"
+    }
+
+    private var isDestroyed: Boolean = false
+
+    override fun onHandleIntent(intent: Intent?) {
+        isDestroyed = false
+        showToast(getString(R.string.starting_intent_service_msg))
+        var i = 0
+        while (i <= 100 && !isDestroyed) {
+            SystemClock.sleep(100)
+            val broadcastIntent =
+                Intent(BGServiceActivity.PROGRESS_UPDATE_ACTION)
+            broadcastIntent.putExtra(
+                BGServiceActivity.PROGRESS_VALUE_KEY,
+                i
+            )
+            sendBroadcast(broadcastIntent)
+            i++
+        }
+        showToast(getString(R.string.finishing_intent_service_msg))
+    }
+
+    protected fun showToast(msg: String) {
+        val intent = Intent(BGServiceActivity.PROGRESS_UPDATE_ACTION)
+        intent.putExtra(BGServiceActivity.SERVICE_STATUS, msg)
+        sendBroadcast(intent)
+    }
+
+    override fun onDestroy() {
+        isDestroyed = true
+        super.onDestroy()
+    }
+}
