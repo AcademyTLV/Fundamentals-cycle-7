7609e79cc3bdb6fd7dbea0dd0be9316038d0a315
Yamin <Yamin.elmakis@here.com>
2019-12-06 00:49:15 +0200

    minor change

--- app/src/main/java/com/android/academy/list/MovieActivity.kt
+++ app/src/main/java/com/android/academy/list/MovieActivity.kt
@@ -2,24 +2,22 @@ package com.android.academy.list
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.android.academy.background_services.BGServiceActivity
 import com.android.academy.R
+import com.android.academy.background_services.BGServiceActivity
 import com.android.academy.background_services.WorkerActivity
 import com.android.academy.details.DetailsActivity
 import com.android.academy.model.MovieModelConverter
 import com.android.academy.model.MoviesContent.movies
-import com.android.academy.threads.AsyncTaskActivity
-import com.android.academy.threads.ThreadsActivity
 import com.android.academy.networking.MoviesListResult
 import com.android.academy.networking.RestClient
+import com.android.academy.threads.AsyncTaskActivity
+import com.android.academy.threads.ThreadsActivity
 import kotlinx.android.synthetic.main.activity_movies.*
 import retrofit2.Call
 import retrofit2.Callback
--- app/src/main/java/com/android/academy/model/MovieModel.kt
+++ app/src/main/java/com/android/academy/model/MovieModel.kt
@@ -30,5 +30,4 @@ object MovieModelConverter {
             )
         }
     }
-
 }
