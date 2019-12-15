fc56a57708dfbdc1cf24a875008ec5f1a8378570
ak3140 <ak3140@intl.att.com>
2019-12-15 08:12:30 +0200

    MoviesDiffUtilCallback - based on movieId now

--- app/src/main/java/com/android/academy/ui/list/MoviesDiffUtilCallback.kt
+++ app/src/main/java/com/android/academy/ui/list/MoviesDiffUtilCallback.kt
@@ -3,15 +3,13 @@ package com.android.academy.ui.list
 import androidx.recyclerview.widget.DiffUtil
 import com.android.academy.model.MovieModel
 
-class MoviesDiffUtilCallback : DiffUtil.ItemCallback<MovieModel>() {
+private class MoviesDiffUtilCallback : DiffUtil.ItemCallback<MovieModel>() {
 
     override fun areItemsTheSame(oldItem: MovieModel, newItem: MovieModel): Boolean {
-        return oldItem.hashCode() == newItem.hashCode()
+        return oldItem.movieId == newItem.movieId
     }
 
     override fun areContentsTheSame(oldItem: MovieModel, newItem: MovieModel): Boolean {
-        return oldItem.imageUrl == newItem.imageUrl
-                && oldItem.name == newItem.name
-                && oldItem.overview == newItem.overview
+        return oldItem == newItem
     }
 }
